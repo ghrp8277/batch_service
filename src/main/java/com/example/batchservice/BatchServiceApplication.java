b517948cc78446fc9f1ea9ec04feeeefd83edac0
august <ghrp8277@naver.com>
2024-08-02 17:05:32 +0900

    a

--- src/main/java/com/example/batchservice/BatchServiceApplication.java
+++ src/main/java/com/example/batchservice/BatchServiceApplication.java
@@ -1,5 +1,6 @@
 package com.example.batchservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -15,6 +16,12 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class BatchServiceApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+
         SpringApplication.run(BatchServiceApplication.class, args);
     }
 
